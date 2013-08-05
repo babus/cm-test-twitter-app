ac5b2f167f6c29930e64e9d7fcfcbb99ff0b8784
Babu Somasundaram <babus@qburst.com>
2013-08-05 20:55:47 +0530

    Initial commit

---
+++ cm_twitter/local_settings.py
@@ -0,0 +1,13 @@
+"""
+This file holds the app specific settings.
+"""
+
+####### Django Allauth settings #######
+SOCIALACCOUNT_AVATAR_SUPPORT = "avatar"
+
+####### Python-twitter Settings #######
+ACCESS_TOKEN = "1639788139-ncJxuX3WuoggOcVx1LZm4f0bcFfJQT8a4OSeBH2"
+ACCESS_SECRET = "8VDiPNZ5ZEPFwVcUyHv3aTqfkkPzNxoUpsvd9P3s"
+
+####### Twitter Face app settings #######
+TAXONAMY_FILE_NAME = "PythonCodeTestSampleTaxonomy.csv"
